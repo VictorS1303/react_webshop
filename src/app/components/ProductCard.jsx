d450b87f9a39a7cfee330d532728c793bedb6c17
Victor Schweitz <victorschweitz@hotmail.com>
2024-11-11 19:59:09 +0100

    ProductCard finished

---
+++ src/app/components/ProductCard.jsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import heroBg from '../images/hero_bg.jpg'
+
+export default function ProductCard() 
+{
+    return (
+
+        <>
+
+            <article className="relative border-2 border-green-300 h-[300px] w-fit p-2 rounded-md transition ease-in duration-150 hover:shadow-md">
+            <header className="group relative header product-card-header">
+                <Image src={heroBg} className="rounded-md" height={140} alt="product card image" />
+                <span className="label amount-label absolute top-2 right-2 bg-green-500 text-white w-10 text-center rounded-md">40</span>
+            </header>
+
+            <div className="product-card-description mt-3">
+                <div className="flex justify-between items-center align-center gap-5">
+                    <div>
+                        <h3 className="text-green-500 font-bold">Category</h3>
+                        <span className="text-black">Beauty</span>
+                    </div>
+                    <div className="text-right">
+                        <h3 className="text-green-500 font-bold">Price</h3>
+                        <span className="text-black">$9.99</span>
+                    </div>
+                </div>
+            </div>
+            <Link href="#" className="absolute bottom-4 left-[50%] -translate-x-[50%] bg-green-500 text-white p-2 w-fit whitespace-nowrap rounded-md cursor-pointer">Show Product</Link>
+            </article>
+        
+        </>
+    ) 
+
+}
--- src/app/components/ProductCardsContainer.jsx
+++ src/app/components/ProductCardsContainer.jsx
@@ -1,12 +1,13 @@
 import Section from './Section.jsx'
+import ProductCard from './ProductCard.jsx'
 
 export default function ProductCardsContainer() 
 {
     return (
         <div className="flex justify-center items-center h-screen pt-[70px]">
             <Section>
-                <div className="border-2 border-black h-[700px] w-[80vw]">
-                    {/* Your content here */}
+                <div className="border-2 border-black h-[700px] w-[80vw] p-6 flex justify-center gap-14 flex-wrap overflow-x-auto">
+                   <ProductCard />
                 </div>
             </Section>
         </div>
