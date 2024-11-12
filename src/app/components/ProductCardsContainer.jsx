f25b0a62bda07050c4fb6b80a22e4429ec361bb3
Victor Schweitz <victorschweitz@hotmail.com>
2024-11-12 08:45:52 +0100

    Minor updates

--- src/app/components/ProductCardsContainer.jsx
+++ src/app/components/ProductCardsContainer.jsx
@@ -8,7 +8,7 @@ export default function ProductCardsContainer()
     return (
         <div className="flex justify-between items-center h-screen pt-[70px] px-48">
             <Section>
-                <div className="border-2 border-green-300 rounded-md h-[700px] w-[50vw] p-6 flex flex-grow-1 justify-center gap-14 flex-wrap overflow-x-auto">
+                <div className="border-2 border-green-300 rounded-md h-[700px] w-[50vw] p-6 justify-center gap-14 flex-wrap overflow-x-auto">
                    <ProductCard />
                 </div>
             </Section>
