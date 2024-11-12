d2cc64fbf2cb6e786d3a8ea6130a6dcb5221ac22
Victor Schweitz <victorschweitz@hotmail.com>
2024-11-12 08:36:54 +0100

    Nothing

--- src/app/components/ProductCardsContainer.jsx
+++ src/app/components/ProductCardsContainer.jsx
@@ -3,8 +3,9 @@ import ProductCard from './ProductCard.jsx'
 
 export default function ProductCardsContainer() 
 {
+    
     return (
-        <div className="flex justify-center items-center h-screen pt-[70px]">
+        <div className="flex justify-between items-center h-screen pt-[70px]">
             <Section>
                 <div className="border-2 border-black h-[700px] w-[80vw] p-6 flex justify-center gap-14 flex-wrap overflow-x-auto">
                    <ProductCard />
