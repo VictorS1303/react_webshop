17a326062148b4110a2df79da3c62f2baf76b1aa
Victor Schweitz <victorschweitz@hotmail.com>
2024-11-12 09:49:58 +0100

    Showcase finished

--- src/app/singleview/page.jsx
+++ src/app/singleview/page.jsx
@@ -1,11 +1,13 @@
 'use client'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import Image from 'next/image'
+import heroBg from '../images/hero_bg.jpg'
 import Navbar from "../components/Navbar.jsx"
+import Showcase from "../components/Showcase.jsx"
+
 
 export default function Product() 
 {
-    const currentPath = usePathname()
     return (
         <>
             <Navbar>
@@ -19,6 +21,33 @@ export default function Product()
                     </li>
                 </ul>
             </Navbar>
+
+
+            <Showcase>
+                <div className="flex items-center justify-between py-4 border-2 border-green-500 h-[450px] w-[800px] mx-auto mt-16 rounded-lg">
+                    <div className="product-images-container mx-12 flex flex-col">
+                        <Image
+                            src={heroBg}
+                            height={400}
+                            width={400}
+                            className="w-full"
+                        />
+                        <div class="thumbnails-container flex justify-between mt-4">
+                            <Image className="rounded-md cursor-pointer transition ease-in duration-150 hover:opacity-85" src={heroBg} height={120} width={120} />
+                            <Image className="rounded-md cursor-pointer transition ease-in duration-150 hover:opacity-85" src={heroBg} height={120} width={120} />
+                            <Image className="rounded-md cursor-pointer transition ease-in duration-150 hover:opacity-85" src={heroBg} height={120} width={120} />
+                        </div>
+                    </div>
+                    <div className="self-start mt-12 w-1/2">
+                        <h1 className="text-green-500 text-2xl">
+                            Product Name
+                        </h1>
+                        <p className="product-description text-black mt-2">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloribus.
+                        </p>
+                    </div>
+                </div>
+            </Showcase>
         </>
     ) 
 }
